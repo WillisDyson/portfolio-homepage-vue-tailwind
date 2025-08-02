fcd44f9176370ad69782ac2654b95a338526070b
Willis Dyson <willisdyson@gmail.comD>
2025-08-02 21:55:35 +0100

    Installing tailwind prettier and formatting component files

--- my-vue-app/src/components/generic/DropdownMenu.vue
+++ my-vue-app/src/components/generic/DropdownMenu.vue
@@ -1,50 +1,52 @@
 <script setup>
 const props = defineProps({
-    text: {
-        type: String,
-        required: true
-    },
-    options: {
-        type: Array,
-        required: true
-    }
+  text: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: Array,
+    required: true,
+  },
 });
 </script>
 
 <template>
+  <div
+    class="group mt-[16px] flex flex-col self-start overflow-hidden rounded-[18px] bg-[#03874b] outline outline-2 outline-transparent focus-within:bg-[#006b3b] hover:bg-[#006b3b] focus:bg-[#006b3b] focus-visible:bg-[#006b3b]"
+    tabindex="0"
+    role="combobox"
+  >
     <div
-        class="group flex flex-col self-start bg-[#03874b] mt-[16px] rounded-[18px] outline outline-2 outline-transparent overflow-hidden
-               hover:bg-[#006b3b] focus:bg-[#006b3b] focus-visible:bg-[#006b3b] focus-within:bg-[#006b3b]"
-        tabindex="0"
-        role="combobox"
+      class="flex items-center px-[22px] py-[7.5px] focus-within:bg-[#006b3b] hover:cursor-pointer hover:bg-[#006b3b] focus:bg-[#006b3b] focus-visible:bg-[#006b3b]"
     >
-        <div
-            class="flex items-center px-[22px] py-[7.5px] hover:cursor-pointer
-                   hover:bg-[#006b3b] focus:bg-[#006b3b] focus-visible:bg-[#006b3b] focus-within:bg-[#006b3b]"
-        >
-            <span>{{ text }}</span>
-            <svg
-                class="ml-[10px] relative top-[0.5px] transition-transform duration-300 rotate-180
-                       group-hover:rotate-0 group-focus:rotate-0 group-focus-visible:rotate-0 group-focus-within:rotate-0"
-                xmlns="http://www.w3.org/2000/svg"
-                width="10.634"
-                height="6.454"
-                viewBox="0 0 10.634 6.454"
-            >
-                <path id="Icon_awesome-chevron-up" data-name="Icon awesome-chevron-up" d="M6.128,8.854l4.733,4.733a.584.584,0,0,1,0,.827l-.552.552a.584.584,0,0,1-.826,0L5.715,11.216,1.947,14.967a.584.584,0,0,1-.826,0l-.552-.552a.584.584,0,0,1,0-.827L5.3,8.854A.584.584,0,0,1,6.128,8.854Z" transform="translate(-0.398 -8.683)" fill="#fff"></path>
-            </svg>
-        </div>
-        <ul
-            class="hidden flex-col bg-white overflow-hidden text-left transition-all duration-800 max-w-full
-                   group-hover:flex group-focus:flex group-focus-visible:flex group-focus-within:flex"
-        >
-            <li
-                v-for="option in options"
-                :key="option.href"
-                class="text-[#101010] px-[22px] py-[12px] decoration-none hover:bg-[#f0f0f0] focus:bg-[#f0f0f0] focus-visible:bg-[#f0f0f0] focus-within:bg-[#f0f0f0]"
-            >
-                <a :href="option.href">{{ option.text }}</a>
-            </li>
-        </ul>
+      <span>{{ text }}</span>
+      <svg
+        class="relative top-[0.5px] ml-[10px] rotate-180 transition-transform duration-300 group-focus-within:rotate-0 group-hover:rotate-0 group-focus:rotate-0 group-focus-visible:rotate-0"
+        xmlns="http://www.w3.org/2000/svg"
+        width="10.634"
+        height="6.454"
+        viewBox="0 0 10.634 6.454"
+      >
+        <path
+          id="Icon_awesome-chevron-up"
+          data-name="Icon awesome-chevron-up"
+          d="M6.128,8.854l4.733,4.733a.584.584,0,0,1,0,.827l-.552.552a.584.584,0,0,1-.826,0L5.715,11.216,1.947,14.967a.584.584,0,0,1-.826,0l-.552-.552a.584.584,0,0,1,0-.827L5.3,8.854A.584.584,0,0,1,6.128,8.854Z"
+          transform="translate(-0.398 -8.683)"
+          fill="#fff"
+        ></path>
+      </svg>
     </div>
+    <ul
+      class="duration-800 hidden max-w-full flex-col overflow-hidden bg-white text-left transition-all group-focus-within:flex group-hover:flex group-focus:flex group-focus-visible:flex"
+    >
+      <li
+        v-for="option in options"
+        :key="option.href"
+        class="decoration-none px-[22px] py-[12px] text-[#101010] focus-within:bg-[#f0f0f0] hover:bg-[#f0f0f0] focus:bg-[#f0f0f0] focus-visible:bg-[#f0f0f0]"
+      >
+        <a :href="option.href">{{ option.text }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
--- my-vue-app/src/components/navigation/NavBar.vue
+++ my-vue-app/src/components/navigation/NavBar.vue
@@ -1,41 +1,49 @@
 <script setup>
-import { ref } from 'vue';
-import NavLink from './NavLink.vue';
-import DropdownMenu from '../generic/DropdownMenu.vue';
+import { ref } from "vue";
+import NavLink from "./NavLink.vue";
+import DropdownMenu from "../generic/DropdownMenu.vue";
 
 const navOptions = [
-    { text: 'About', href: '#about' },
-    { text: 'Skills', href: '#skills' },
-    { text: 'Certifications', href: '#certifs' },
-    { text: 'Career & Education', href: '#career' },
-    { text: 'Projects', href: '#work' },
-    { text: 'Social Media & Contact', href: '#contact' }
+  { text: "About", href: "#about" },
+  { text: "Skills", href: "#skills" },
+  { text: "Certifications", href: "#certifs" },
+  { text: "Career & Education", href: "#career" },
+  { text: "Projects", href: "#work" },
+  { text: "Social Media & Contact", href: "#contact" },
 ];
 
 const dropdownOptions = [
-    { text: 'Screen-friendly PDF', href: '/assets/willis-dyson__cv-july-2025-interactive.pdf' },
-    { text: 'Print-friendly PDF', href: '/assets/willis-dyson__cv-july-2025-print.pdf' }
+  {
+    text: "Screen-friendly PDF",
+    href: "/assets/willis-dyson__cv-july-2025-interactive.pdf",
+  },
+  {
+    text: "Print-friendly PDF",
+    href: "/assets/willis-dyson__cv-july-2025-print.pdf",
+  },
 ];
 
 const activeHref = ref(navOptions[0].href);
 
 function setActive(href) {
-    activeHref.value = href;
+  activeHref.value = href;
 }
 </script>
 
 <template>
-    <nav class="bg-custom-dark-gray">
-        <ul class="flex items-center justify-evenly max-w-[1600px] h-[67px] mx-auto">
-            <NavLink
-                v-for="option in navOptions"
-                :key="option.href"
-                :text="option.text"
-                :href="option.href"
-                :active="activeHref === option.href"
-                @click="setActive(option.href)"
-            />
-            <DropdownMenu text="Download my CV" :options="dropdownOptions" />
-        </ul>
-    </nav>
+  <nav class="bg-custom-dark-gray">
+    <ul
+      class="mx-auto flex h-[67px] max-w-[1600px] items-center justify-evenly"
+    >
+      <NavLink
+        v-for="option in navOptions"
+        :key="option.href"
+        :text="option.text"
+        :href="option.href"
+        :active="activeHref === option.href"
+        @click="setActive(option.href)"
+      />
+      <DropdownMenu text="Download my CV" :options="dropdownOptions" />
+    </ul>
+  </nav>
 </template>
