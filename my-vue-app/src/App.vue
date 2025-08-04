6764948ba12ccd202c4862dd8e6742efbacc84e8
Willis Dyson <willisdyson@gmail.comD>
2025-08-04 20:15:57 +0100

    Moving all images to public folder

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -39,19 +39,19 @@ const dropdownOptions = [
 ];
 
 const heroImgData = {
-  url: "images/me.png",
+  url: "/images/me.png",
   alt: "Photograph of Willis. He has short brown hair, glasses, and short facial hair. He is smiling while wearing a beige T-shirt and looking straight on at the camera.",
 };
 
 const h1textData = {
   name: "Willis Dyson",
-  image: { url: "images/wave.png", alt: "Waving hand" },
+  image: { url: "/images/wave.png", alt: "Waving hand" },
 };
 
 const sublineData = {
   roles: ["Front End UI Developer", "Web Accessibility Lead"],
   location: "Leeds, UK",
-  image: { url: "images/uk.png", alt: "UK flag" },
+  image: { url: "/images/uk.png", alt: "UK flag" },
 };
 
 const skillCardData = [
@@ -75,14 +75,14 @@ const skillCardData = [
 const seeMoreHref = "#skills";
 
 const skillGridData = [
-  { src: "images/html.png", text: "HTML" },
-  { src: "images/sass.png", text: "SASS (SCSS)" },
-  { src: "images/css.png", text: "CSS" },
-  { src: "images/js.png", text: "JavaScript" },
-  { src: "images/git.png", text: "Git" },
-  { src: "images/react.png", text: "React" },
-  { src: "images/tw.png", text: "Tailwind" },
-  { src: "images/vue.png", text: "Vue.js" },
+  { src: "/images/html.png", text: "HTML" },
+  { src: "/images/sass.png", text: "SASS (SCSS)" },
+  { src: "/images/css.png", text: "CSS" },
+  { src: "/images/js.png", text: "JavaScript" },
+  { src: "/images/git.png", text: "Git" },
+  { src: "/images/react.png", text: "React" },
+  { src: "/images/tw.png", text: "Tailwind" },
+  { src: "/images/vue.png", text: "Vue.js" },
 ];
 
 const skillListDesignData = [
@@ -109,90 +109,90 @@ const certificationData = [
     title: "Introduction to Vue.js framework",
     issuer: "Codio",
     completed: "July 2025",
-    logoUrl: "images/codio.png",
+    logoUrl: "/images/codio.png",
   },
   {
     url: "https://www.coursera.org/account/accomplishments/verify/OBJXXKUK73GY",
     title: "Learn Tailwind CSS",
     issuer: "Scrimba",
     completed: "July 2025",
-    logoUrl: "images/scrimba.png",
+    logoUrl: "/images/scrimba.png",
   },
   {
     url: "https://www.coursera.org/account/accomplishments/verify/9HKUP9XT5HGW",
     title: "Advanced React",
     issuer: "Meta",
     completed: "April 2024",
-    logoUrl: "images/logo-Meta.png",
+    logoUrl: "/images/logo-Meta.png",
   },
   {
     url: "https://www.coursera.org/account/accomplishments/verify/F5ERDPRB6SEY",
     title: "React Basics",
     issuer: "Meta",
     completed: "January 2024",
-    logoUrl: "images/logo-Meta.png",
+    logoUrl: "/images/logo-Meta.png",
   },
   {
     url: "https://www.coursera.org/account/accomplishments/verify/GN4LFM5RTS4C",
     title: "Programming with JavaScript",
     issuer: "Meta",
     completed: "November 2023",
-    logoUrl: "images/logo-Meta.png",
+    logoUrl: "/images/logo-Meta.png",
   },
   {
     url: "https://courses.edx.org/certificates/1921f1cc711447a3aeed6edf61508c12",
     title: "Introduction to Web Accessibility",
     issuer: "W3C",
     completed: "September 2023",
-    logoUrl: "images/w3c-white.png",
+    logoUrl: "/images/w3c-white.png",
   },
   {
     url: "https://www.credly.com/badges/802c6142-fd3a-41a2-84dd-8196f056d8a7/public_url",
     title: "AgilePM® Foundation",
     issuer: "APMG",
     completed: "October 2022",
-    logoUrl: "images/apmg-white.png",
+    logoUrl: "/images/apmg-white.png",
   },
   {
     url: "https://www.coursera.org/account/accomplishments/verify/8D5L6K7CWHL4",
     title: "Foundations of User Experience (UX) Design",
     issuer: "Google",
     completed: "August 2021",
-    logoUrl: "images/google.png",
+    logoUrl: "/images/google.png",
   },
 ];
 
 const careerData = [
   {
-    logoUrl: "images/tcb.png",
+    logoUrl: "/images/tcb.png",
     alt: "TopCashback",
     location: "Stafford, West Midlands, UK",
     title: "Front End UI Developer & Web Accessibility Lead",
     dates: "Current role since March 2025",
   },
   {
-    logoUrl: "images/tcb.png",
+    logoUrl: "/images/tcb.png",
     alt: "TopCashback",
     location: "Stafford, West Midlands, UK",
     title: "Front End UI Developer",
     dates: "Feb 2022 - Mar 2025",
   },
   {
-    logoUrl: "images/ebuyer.png",
+    logoUrl: "/images/ebuyer.png",
     alt: "Ebuyer",
     location: "Howden, East Yorkshire, UK",
     title: "UI Designer/Developer",
     dates: "May 2021 - Feb 2022",
   },
   {
-    logoUrl: "images/uoh.png",
+    logoUrl: "/images/uoh.png",
     alt: "University of Huddersfield",
     location: "Huddersfield, West Yorkshire, UK",
     title: "1st Class Information & Communication Technology (BSc) Graduate",
     dates: "May 2021",
   },
   {
-    logoUrl: "images/tcb.png",
+    logoUrl: "/images/tcb.png",
     alt: "TopCashback",
     location: "Stafford, West Midlands, UK",
     title: "UX Designer (12 months internship)",
@@ -205,28 +205,28 @@ const projectData = [
     name: "Vue.js and Tailwind URL Shortener practice app",
     tags: ["Vue.js", "Tailwind", "API", "In progress"],
     tagsClass: ["tag-vuejs", "tag-tailwind", "tag-api", "tag-in-progress"],
-    backgroundImage: "bg-[url(images/vuetw.png)]",
+    backgroundImage: "bg-[url(/images/vuetw.png)]",
     pageUrl: "https://willisdyson.dev/vuetw-proj",
   },
   {
     name: "Rolling Block – Accessibility services React app",
     tags: ["UX Design", "React", "API", "In progress"],
     tagsClass: ["tag-ux-design", "tag-react", "tag-api", "tag-in-progress"],
-    backgroundImage: "bg-[url(images/rb-tile.png)]",
+    backgroundImage: "bg-[url(/images/rb-tile.png)]",
     pageUrl: "https://willisdyson.dev/rolling-block",
   },
   {
     name: "Brand Pages Redesign – TopCashback",
     tags: ["Vanilla JS", "UX Design", "Accessibility"],
     tagsClass: ["tag-vanilla-js", "tag-ux-design", "tag-accessibility"],
-    backgroundImage: "bg-[url(images/merch-pages.png)]",
+    backgroundImage: "bg-[url(/images/merch-pages.png)]",
     pageUrl: "https://willisdyson.dev/brand-page-redesign",
   },
   {
     name: "Sitewide Navigation Redesign – TopCashback",
     tags: ["Vanilla JS", "UX Design"],
     tagsClass: ["tag-vanilla-js", "tag-ux-design"],
-    backgroundImage: "bg-[url(images/new-nav.png)]",
+    backgroundImage: "bg-[url(/images/new-nav.png)]",
     pageUrl: "https://willisdyson.dev/nav-redesign",
   },
 ];
